c367de71345271463fa4e683b597c1432568b246
ta-sakin <tahjib.alsakin@gmail.com>
2025-08-23 20:58:40 +0600

    added vercel.json

--- nhost/functions/utils/nhost-admin.ts
+++
@@ -1,8 +0,0 @@
-import { NhostClient } from "@nhost/nhost-js";
-
-// Admin client for server-side operations
-export const nhost = new NhostClient({
-  subdomain: process.env.NHOST_SUBDOMAIN || "localhost",
-  region: process.env.NHOST_REGION || "local",
-  adminSecret: process.env.NHOST_ADMIN_SECRET || "nhost-admin-secret",
-});
